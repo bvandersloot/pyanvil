6a45330b70c3c59d572b744ce238f17f68cbeab4
Benjamin VanderSloot <benvds@umich.edu>
2019-10-10 07:44:08 -0400

    Downloading data quickly

--- src/anvil.py
+++ src/anvil.py
@@ -1,14 +1,19 @@
 import argparse
+from io import StringIO
 import simplejson as json
 from dateutil.parser import isoparse
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import sys
+import psycopg2
+from psycopg2.extras import execute_values
+from _pybgpstream import BGPStream, BGPRecord, BGPElem
+import itertools
 
 WINDOW = timedelta(days=10)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Anomaly detection in BGP+ACME data')
-    parser.add_argument('--database', '-d', type=str, nargs=1, default="localhost:5432", help='Database network location')
+    parser.add_argument('--database', '-d', type=str, nargs=1, default="localhost", help='Database network location')
     parser.add_argument('--input', '-i', type=str, nargs=1, default='./in.jsonl', help='Input certificate requests to classify')
     parser.add_argument('--update_database', '-u', action="store_true", default=False)
     return parser.parse_args()
@@ -60,13 +65,62 @@ def get_time_range(args):
                         end = address['time']
     return (start-WINDOW, end)
 
-def update_bgp(args):
+def get_ribs(collector, start, period):
+    stream = BGPStream()
+    rec = BGPRecord()
+    stream.add_filter('collector',collector)
+    stream.add_interval_filter(int(start.timestamp()) - period,int(start.timestamp()))
+    stream.add_filter('record-type', 'ribs')
+    stream.start()
+    while(stream.get_next_record(rec)):
+        if rec.status == "valid":
+            elem = rec.get_next_elem()
+            while(elem):
+                yield (rec.project, rec.collector, datetime.utcfromtimestamp(rec.time).isoformat(), elem.peer_address, elem.peer_asn, elem.fields['prefix'], elem.fields['as-path'])
+                elem = rec.get_next_elem()
 
-    pass
+def update_bgp(args, start, end):
+    conn = psycopg2.connect('host={} user=postgres password=example'.format(args.database))
+    cur = conn.cursor()
+    cur.execute('select * from bgp_metadata;')
+    rows = cur.fetchall()
+    for row in rows:
+        (project, collector, period, old_start, old_end) = row
+        if old_start != None and old_end != None:
+            old_start = isoparse(old_start)
+            old_end = isoparse(old_end)
+            if start >= old_start and end <= old_end:
+                continue
+        else:
+            old_start = datetime(1970,1,1,tzinfo=timezone.utc)
+            old_end = datetime(1970,1,1,tzinfo=timezone.utc)
+        if old_start > start or old_end + timedelta(days=1) < start:
+            cur.execute('delete from bgp_rib where project = %s and collector = %s', (project, collector))
+            i = 0
+            f = StringIO()
+            for value in get_ribs(collector, start, period):
+                i+=1
+                f.write('\t'.join(map(str, value))+'\n')
+                if i % 1000000 == 0:
+                    f.seek(0)
+                    cur.copy_from(file=f, table='bgp_rib', columns=('project', 'collector', 'time', 'peer_addr', 'peer_asn', 'prefix', 'as_path'))
+                    f = StringIO()
+                    print(i)
+        #extend end
+        #roll beginning
+        print(row)
+    conn.close()
 
 def main():
     args = parse_args()
+    #load model
     (start, end) = get_time_range(args)
+    update_bgp(args, start, end)
+    #for each input
+        #extract features
+        #get score
+    #get N most aberrant issuances and links to RIPEstat
+    #dump certificates
 
 if __name__== "__main__":
     main()
